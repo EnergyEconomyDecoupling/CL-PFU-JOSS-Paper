56c90ded16e8779a8a1b1bad26c2419343354845
Matthew Heun <matthew.heun@me.com>
2023-08-08 16:16:12 -0400

    Working on example.

--- ExampleFolder/_targets_pfudatabase.R
+++ ExampleFolder/_targets_pfudatabase.R
@@ -1,20 +1,30 @@
 library(magrittr)
-library(targets)
 library(PFUDatabase)
+library(PFUSetup)
 # targets::tar_make() to run the pipeline
 # targets::tar_make_future(workers = 8) to execute across multiple cores.
 # targets::tar_read(<<target_name>>) to view the results.
 # targets::tar_destroy() to start over with everything.
 # targets::tar_make(callr_function = NULL) to debug.
 
-
-# Input data version
-version <- "v_example"
+# Use the PFUSetup package to establish paths for the example
+home_path <- "ExampleFolder"
+iea_folder_path <- file.path(home_path, "IEA extended energy balance data")
+iea_data_path <- file.path("IEA extended energy balance data",
+                           "GHA-ZAF-TJ-Extended-Energy-Balances-sample-2022.csv")
+# Create the pfu_setup object used in the targets scripts 
+# _targets_pfudatabase.R and _targets_pfuaggdatabase.R.
+pfu_setup <- PFUSetup::get_abs_paths(home_path = file.path("ExampleFolder"), 
+                                     dropbox_path = "",
+                                     project_path = "",
+                                     iea_folder_path = iea_folder_path,
+                                     iea_data_path = iea_data_path,
+                                     version = "v_example")
 
 # Custom parameters
 years <- c(1971, 2000)      # The years to be analyzed
 
-countries <- c("BHA", "ZAF")
+countries <- c("GHA", "ZAF")
 
 
 # Additional exemplar countries are countries which aren't included in the workflow
@@ -74,22 +84,22 @@ PFUDatabase::get_pipeline(countries = countries,
                           apply_fixes = apply_fixes,
                           years = years,
                           how_far = "all_targets",
-                          iea_data_path = PFUSetup::get_abs_paths(version = version)[["iea_data_path"]],
-                          country_concordance_path = PFUSetup::get_abs_paths(version = version)[["country_concordance_path"]],
-                          mw_concordance_path = PFUSetup::get_abs_paths(version = version)[["mw_concordance_path"]],
-                          amw_analysis_data_path = PFUSetup::get_abs_paths(version = version)[["amw_analysis_data_path"]],
-                          hmw_analysis_data_path = PFUSetup::get_abs_paths(version = version)[["hmw_analysis_data_path"]],
-                          phi_constants_path = PFUSetup::get_abs_paths(version = version)[["phi_constants_path"]],
+                          iea_data_path = pfu_setup[["iea_data_path"]],
+                          country_concordance_path = pfu_setup[["country_concordance_path"]],
+                          mw_concordance_path = pfu_setup[["mw_concordance_path"]],
+                          amw_analysis_data_path = pfu_setup[["amw_analysis_data_path"]],
+                          hmw_analysis_data_path = pfu_setup[["hmw_analysis_data_path"]],
+                          phi_constants_path = pfu_setup[["phi_constants_path"]],
                           # Temperature data not required for V1, argument set to NULL.
                           ceda_data_folder = NULL,
-                          fao_data_path = PFUSetup::get_abs_paths(version = version)[["fao_data_path"]],
-                          ilo_data_path = PFUSetup::get_abs_paths(version = version)[["ilo_data_path"]],
-                          machine_data_path = PFUSetup::get_abs_paths(version = version)[["machine_data_folder"]],
-                          exemplar_table_path = PFUSetup::get_abs_paths(version = version)[["exemplar_table_path"]],
-                          fu_analysis_folder = PFUSetup::get_abs_paths(version = version)[["fu_analysis_folder"]],
-                          reports_source_folders = PFUSetup::get_abs_paths(version = version)[["reports_source_folders"]],
-                          reports_dest_folder = PFUSetup::get_abs_paths(version = version)[["reports_dest_folder"]],
-                          pipeline_releases_folder = PFUSetup::get_abs_paths(version = version)[["pipeline_releases_folder"]],
-                          pipeline_caches_folder = PFUSetup::get_abs_paths(version = version)[["pipeline_caches_folder"]],
+                          fao_data_path = pfu_setup[["fao_data_path"]],
+                          ilo_data_path = pfu_setup[["ilo_data_path"]],
+                          machine_data_path = pfu_setup[["machine_data_folder"]],
+                          exemplar_table_path = pfu_setup[["exemplar_table_path"]],
+                          fu_analysis_folder = pfu_setup[["fu_analysis_folder"]],
+                          reports_source_folders = pfu_setup[["reports_source_folders"]],
+                          reports_dest_folder = pfu_setup[["reports_dest_folder"]],
+                          pipeline_releases_folder = pfu_setup[["pipeline_releases_folder"]],
+                          pipeline_caches_folder = pfu_setup[["pipeline_caches_folder"]],
                           release = release)
 
